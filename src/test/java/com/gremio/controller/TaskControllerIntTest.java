8e3e1161983312b1dc65bb10ea3dde22c75f259d
Urban Roland <urolir@gmail.com>
2024-01-12 18:33:29 +0100

    update TaskControllerIntTest,mock some data

--- src/test/java/com/gremio/controller/TaskControllerIntTest.java
+++ src/test/java/com/gremio/controller/TaskControllerIntTest.java
@@ -1,29 +1,59 @@
 package com.gremio.controller;
 
 import com.gremio.enums.TaskStatus;
+import com.gremio.model.dto.TaskDto;
+import com.gremio.persistence.entity.Project;
 import com.gremio.persistence.entity.Task;
+import com.gremio.service.TaskServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.graphql.tester.AutoConfigureGraphQlTester;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.graphql.test.tester.GraphQlTester;
 import org.springframework.security.test.context.support.WithMockUser;
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+//test over http
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureGraphQlTester
 public class TaskControllerIntTest {
     @Autowired
     GraphQlTester graphQlTester;
     
+    @MockBean
+    private TaskServiceImpl taskService;
+    private Project project;
+    private LocalDateTime localDateTime;
+
+    @BeforeEach
+    void setup() {
+        project = new Project();
+        project.setId(1L);
+        localDateTime = LocalDateTime.of(2023, 7, 9, 11, 54, 42);
+    }
+
     @Test
     @WithMockUser(authorities = "ROLE_ADMIN")
     void TaskController_CreateTask_ReturnTask() {
-       
-       //language=GraphQL
+        Task expectedTask =  Task.builder()
+            .title("test")
+            .status(TaskStatus.PASSED)
+            .description("123testdescruiption")
+            .due(localDateTime)
+            .project(project)
+            .build();
+
+        Mockito.when(taskService.addTask(Mockito.any(TaskDto.class))).thenReturn(expectedTask);
+
+        //language=GraphQL
        String document = """
         mutation {
-          createTask(task:{status:PASSED, title:"test", description:"testdescruiption", due:"2023-07-09T11:54:42", projectId: 1}){
+          createTask(task:{status:PASSED, title:"test", description:"123testdescruiption", due:"2023-07-09T11:54:42", projectId: 1}) {
             title
             description
             status
@@ -31,13 +61,13 @@ public class TaskControllerIntTest {
           }
         }
            """;
-      
-       graphQlTester.document(document).execute().path("createTask").entity(Task.class).satisfies( task -> {
-           assertEquals(task.getStatus(), TaskStatus.PASSED);
-           assertEquals(task.getTitle(), "test");
-           assertEquals(task.getDescription(), "testdescruiption");
-           assertEquals(task.getDue().toString(), "2023-07-09T11:54:42");
-       });
+    
+        graphQlTester.document(document).execute().path("createTask").entity(Task.class).satisfies(task -> {
+            assertEquals(expectedTask.getStatus(), task.getStatus());
+            assertEquals(expectedTask.getTitle(), task.getTitle());
+            assertEquals(expectedTask.getDescription(), task.getDescription());
+            assertEquals(expectedTask.getDue(), task.getDue());
+        });
        
     }
     
